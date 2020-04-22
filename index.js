09dfaccbbe021244b387fc783e339426fb13e298
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-04-21 22:44:19 -0500

    completed task 7 (MVP)

--- index.js
+++ index.js
@@ -4,48 +4,49 @@ import { fifaData } from './fifa.js';
 // ⚽️ M  V P ⚽️ //
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data */
-const final2014 = fifaData.filter( fifa => (fifa.Year===2014 && fifa.Stage==="Final"));
+
+// const finals2014 = fifaData.filter( fifa => (fifa['Year']===2014 && fifa['Stage']==="Final"));
+
+let final2014 = fifaData.filter(game => (game['Year'] == 2014 && game['Stage'] === "Final"))[0]; // [0] at end grabs the first item in new array making it easier for us to access what we want (below) -- would have to do final2014[0]['Home Team Name']
 
 // (a) Home Team name for 2014 world cup final
-// console.log(final2014.map(final => final["Home Team Name"])); 
-console.log(final2014[0]["Home Team Name"]); 
+console.log(`${final2014["Home Team Name"]}`); 
+
 // (b) Away Team name for 2014 world cup final
-// console.log(final2014.map( final => final["Away Team Name"])); 
-console.log(final2014[0]["Away Team Name"]);
+console.log(final2014["Away Team Name"]);
+
 // (c) Home Team goals for 2014 world cup final
-// console.log(final2014.map(final => final["Home Team Goals"]));
-console.log(final2014[0]["Home Team Goals"])
+console.log(final2014["Home Team Goals"])
+
 // (d) Away Team goals for 2014 world cup final
-// console.log(final2014.map(final => final["Away Team Goals"]));
-console.log(final2014[0]["Away Team Goals"]); 
+console.log(final2014["Away Team Goals"]); 
+
 // (e) Winner of 2014 world cup final
-// console.log(final2014.map(final => final["Win conditions"])); 
-console.log(final2014[0]["Win conditions"])
+console.log(final2014["Win conditions"])
 
 
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
 function getFinals(data) {
-    const final = data.filter( fifa => (fifa.Stage==="Final"));
+    const final = data.filter( fifa => fifa.Stage === "Final");
     return final; 
 };
 
-console.log(getFinals(fifaData));
+// console.log(getFinals(fifaData));
 
 /* Task 3: Impliment a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(data, getFinals){
+function getYears( data, getFinals){
     //create array called years to return 
     let years = []; 
     const result = getFinals(data); 
 
     result.forEach(item=> years.push(`${item.Year}`)); 
-
     return years; 
 }; 
 
-console.log(getYears(fifaData, getFinals)); 
+// console.log(getYears(fifaData, getFinals)); 
 
 /* Task 5: Impliment a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
@@ -78,7 +79,7 @@ function getWinners(data, getFinals) {
     
 }; 
 
-console.log(getWinners(fifaData, getFinals)); 
+// console.log(getWinners(fifaData, getFinals)); 
 
 
 
@@ -99,15 +100,16 @@ Parameters:
  */
 
 function getAllWinners(data, getWinners, getYears) {
-    let allWinners = [];
     let years = getYears(data, getFinals); 
     console.log(years); 
     let winners = getWinners(data, getFinals); 
-  
+    console.log(winners);
 
+    // couldn't figure out how to make use of the two arrays above until i tried to find a way to merge them together
+    // found this (below) and realized I could use ${} to make the string whatever I want
+    // https://stackoverflow.com/questions/43170806/merge-multiple-arrays-based-on-their-index-using-javascript
 
-    years.forEach(item => allWinners.push(`In ${item},`));
-    winners.forEach(item => allWinners.push(` ${item} won the World Cup!`)); 
+    let allWinners = years.map((e,i) => `In ${e}, ${winners[i]} won the world cup!`); 
     
     return allWinners; 
 };
@@ -136,13 +138,21 @@ console.log(getAllWinners(fifaData, getWinners, getYears));
 Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
 
-function getCountryWins(/* code here */) {
+function getCountryWins(data, tmI) {
+    // create an array with every game that team played in
+    // going to seperate out home and away games to simplify totally goals at later step
+    const allHomeGames = data.filter( game => game['Home Team Initials'] === tmI);
+    const allAwayGames = data.filter( game => game['Away Team Initials'] === tmI); 
 
-    /* code here */
+    // use reduce to add up total home/away goals respectively
+    const homeGoals = allHomeGames.reduce((acc,item) => acc + item['Home Team Goals'],0); 
+    const awayGoals = allAwayGames.reduce((acc,item) => acc + item['Away Team Goals'],0); 
 
+    // return home + away totals
+    return homeGoals + awayGoals; 
 };
 
-getCountryWins();
+getCountryWins(fifaData, 'BRA');
 
 
 
@@ -186,6 +196,7 @@ badDefense();
 function getAverageGoals(/* code here */) {
 
     /* code here */
+
 
 };
 
